cec7697f4d752f42d8c2828fd3a2ca05de83aadf
Apells1 <57453929+Apells1@users.noreply.github.com>
2021-10-30 18:02:41 -0500

    Add files via upload

---
+++ AlchoholAge/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlchoholAge
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void onAge(object sender, EventArgs e)
+        {
+            int Age = Convert.ToInt32(agein.Text.ToString());
+            int years = 21 - Age;
+            Output.Text = ""+years
+        }
+    }
+}
